03ff301f91d6283856c1783e923e98060f011440
DIVYA SHARMA <divya@racloop.com>
2021-11-02 10:54:58 +0530

    I18nController

--- springcontext/src/main/java/com/springcontextexample/springcontext/services/I18nSpanishGreetingServices.java
+++ springcontext/src/main/java/com/springcontextexample/springcontext/services/I18nSpanishGreetingServices.java
@@ -3,7 +3,7 @@ package com.springcontextexample.springcontext.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+@Profile({"ES", "default"})
 @Service("i18nService")
 public class I18nSpanishGreetingServices implements GreetingServices {
 
